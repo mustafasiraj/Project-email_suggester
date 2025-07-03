3280cf41268aa0d9da92219d10e610ca39cf40f2
mustafasiraj <contact.citychicstore@gmail.com>
2025-07-03 09:01:09 -0700

    Update email_suggester.py

--- email_suggester.py
+++ email_suggester.py
@@ -6,8 +6,8 @@ import torch
 import os
 
 # ========== 1. GMAIL LOGIN ==========
-email_user = 'Your_email_user'
-app_password = 'Your_app_password'  # App password from Google
+email_user = ## Your_email_user'
+app_password = ## 'Your_app_password'  # App password from Google
 
 try:
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
